981c4b6749cd90897d1fc0176688d973d7f6c4ca
MaximeAtticCorp <mmichel@attic-corp.com>
2022-05-28 12:12:59 +0200

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html", {})
+
+
+def about(request):
+    return render(request, "about.html", {})
+
+
+def contact(request):
+    return render(request, "contact.html", {})
+
+
+def work(request):
+    return render(request, "work.html", {})
+
+
+def works(request):
+    return render(request, "works.html", {})
+
+
+def components(request):
+    return render(request, "components.html", {})
+
